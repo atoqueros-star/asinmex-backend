4f7e2f947dffe32f3ba46863b107746f05096b79
atoqueros-star <atoqueros@gmail.com>
2026-06-09 00:39:42 -0600

    Add files via upload

--- aiService.js
+++ aiService.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Inicializamos la API con la llave provista
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 // Prompt del Sistema que define la personalidad y conocimiento del Chatbot
 const SYSTEM_PROMPT = `
